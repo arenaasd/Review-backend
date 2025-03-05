cfe2ce80283fa7dd79856d4ee4e3085504a82bd4
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-06 02:35:26 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const aiRoutes = require('./routes/ai.route')
 const cors = require('cors');
-
-app.use(express.json())
 app.use(cors());
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
